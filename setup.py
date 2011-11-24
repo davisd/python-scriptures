46c7f7b9f1d937582c009bbc7f73c0d37324f911
David Davis <davisd@davisd.com>
2011-11-24 11:24:06 -0600

    Added trailing slash to url in setup script

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='David Davis',
     author_email='davisd@davisd.com',
     packages=['scriptures',],
-    url='http://www.davisd.com/projects/python-scriptures',
+    url='http://www.davisd.com/projects/python-scriptures/',
     data_files=[('.',['LICENSE'])],
     license='LICENSE',
     description='python-scriptures is a Python package and regular ' \
