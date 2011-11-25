b615b367ba658e51abad67abc2a092c2632f5476
David Davis <davisd@davisd.com>
2011-11-25 09:26:37 -0600

    Bumped version to 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='python-scriptures',
-    version='1.0.0',
+    version='1.0.1',
     author='David Davis',
     author_email='davisd@davisd.com',
     packages=['scriptures',],
