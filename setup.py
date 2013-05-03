7a9612c099b2256df0d0bc061a3bf1d45d468568
David Davis <davisd@davisd.com>
2013-05-03 07:15:02 -0500

    bumped version to 1.1.2 and added changelog entry

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='python-scriptures',
-    version='1.1.1',
+    version='1.1.2',
     author='David Davis',
     author_email='davisd@davisd.com',
     packages=['scriptures',],
