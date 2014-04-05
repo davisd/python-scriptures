debc76c8a50272517d6ef9ef37b8c6987000b85e
David Davis <davisd@davisd.com>
2014-04-05 08:39:17 -0500

    simple adjustments for python 3 compatibility

--- scriptures/__init__.py
+++ scriptures/__init__.py
@@ -1,5 +1,5 @@
-from bible_re import book_re, scripture_re
+from .bible_re import book_re, scripture_re
 
-from references import extract, reference_to_string, \
+from .references import extract, reference_to_string, \
   normalize_reference, is_valid_reference
 
--- scriptures/references.py
+++ scriptures/references.py
@@ -1,6 +1,6 @@
 import re
 
-from bible_re import testaments, book_re, scripture_re
+from .bible_re import testaments, book_re, scripture_re
 
 class InvalidReferenceException(Exception):
     """
@@ -12,7 +12,7 @@ def get_book(name):
     """
     Get a book from its name or None if not found
     """
-    for books in testaments.itervalues():
+    for books in testaments.values():
         for book in books:
             if re.match(book[2], name, re.IGNORECASE):
                 return book
