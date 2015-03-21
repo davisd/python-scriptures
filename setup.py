f3012211d752de57c856b50821622be399f56b80
David Davis <davisd@davisd.com>
2015-03-20 21:45:40 -0500

    updated README, CHANGELOG and bumped to 2.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='python-scriptures',
-    version='2.2.0pre',
+    version='2.2.0',
     author='David Davis',
     author_email='davisd@davisd.com',
     packages=['scriptures',],
