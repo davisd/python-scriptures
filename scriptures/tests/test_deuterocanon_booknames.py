2484c8c95cc4537f17a807759bd45ec3c968e0e1
Michael L. Vezie <mlv@pobox.com>
2016-10-01 22:49:34 -0400

    Completed the Deuterocanonical (according to KJV) books.

--- scriptures/tests/test_deuterocanon_booknames.py
+++ scriptures/tests/test_deuterocanon_booknames.py
@@ -2,7 +2,8 @@ import unittest
 
 from ..texts.deuterocanon import Deuterocanon
 
-dc=Deuterocanon()
+dc = Deuterocanon()
+
 
 def f(txt):
     """
@@ -12,59 +13,91 @@ def f(txt):
     return dc.reference_to_string(
             *dc.normalize_reference(*dc.scripture_re.match(txt).groups()))
 
+
 class TestDeuterocanonBookNames(unittest.TestCase):
     def setUp(self):
         pass
+
+    def test_1esd(self):
+        self.assertEqual(f('1esdras 1'),
+                         'I Esdras 1')
+        self.assertEqual(f('1esd 1'),
+                         'I Esdras 1')
+        self.assertEqual(f('Iesdras 1'),
+                         'I Esdras 1')
+
+    def test_2esd(self):
+        self.assertEqual(f('2esdras 1'),
+                         'II Esdras 1')
+        self.assertEqual(f('2esd 1'),
+                         'II Esdras 1')
+        self.assertEqual(f('IIesdras 1'),
+                         'II Esdras 1')
 
     def test_tobit(self):
         self.assertEqual(f('tobit 1:1'), 'Tobit 1:1')
         self.assertEqual(f('tob 1:1'), 'Tobit 1:1')
 
     def test_judith(self):
-        # TODO
-        raise Exception('Not yet implemented')
+        self.assertEqual(f('judith 1:1'), 'Judith 1:1')
+        self.assertEqual(f('Jud 1:1'), 'Judith 1:1')
 
     def test_additions_to_esther(self):
-        # TODO
-        raise Exception('Not yet implemented')
+        self.assertEqual(f('esther (greek) 1:1'), 'Esther (Greek) 1:1')
 
     def test_wisdom_of_solomon(self):
-        # TODO
-        raise Exception('Not yet implemented')
+        self.assertEqual(f('the wisdom of solomon 1:1'),
+                         'The Wisdom of Solomon 1:1')
+        self.assertEqual(f('wis 1:1'), 'The Wisdom of Solomon 1:1')
+        self.assertEqual(f('wisdom of solomon 1:1'),
+                         'The Wisdom of Solomon 1:1')
+        self.assertEqual(f('wisdom 1:1'),
+                         'The Wisdom of Solomon 1:1')
 
     def test_ecclesiasticus_aka_sirach(self):
-        # TODO
-        raise Exception('Not yet implemented')
+        self.assertEqual(f('sirach 1:1'), 'Sirach 1:1')
+        self.assertEqual(f('sir 1:1'), 'Sirach 1:1')
 
     def test_baruch(self):
-        # TODO
-        raise Exception('Not yet implemented')
+        self.assertEqual(f('baruch 1:1'), 'Baruch 1:1')
+        self.assertEqual(f('bar 1:1'), 'Baruch 1:1')
 
     def test_letter_of_jeremiah(self):
-        # TODO
-        raise Exception('Not yet implemented')
+        self.assertEqual(f('letter of jeremiah 1'),
+                         'Letter of Jeremiah 1')
 
     def testprayer_of_azariah(self):
-        # TODO
-        raise Exception('Not yet implemented')
+        self.assertEqual(f('prayer of azariah 1'),
+                         'Prayer of Azariah 1')
+        self.assertEqual(f('azar 1'),
+                         'Prayer of Azariah 1')
 
     def test_susanna(self):
-        # TODO
-        raise Exception('Not yet implemented')
+        self.assertEqual(f('susanna 1'), 'Susanna 1')
 
     def test_bel_and_the_dragon(self):
-        # TODO
-        raise Exception('Not yet implemented')
+        self.assertEqual(f('bel and the dragon 1'),
+                         'Bel and the Dragon 1')
+        self.assertEqual(f('bel 1'),
+                         'Bel and the Dragon 1')
+        self.assertEqual(f('bel dragon 1'),
+                         'Bel and the Dragon 1')
 
     def test_prayer_of_manasseh(self):
-        # TODO
-        raise Exception('Not yet implemented')
+        self.assertEqual(f('prayer of manasseh 1'),
+                         'Prayer of Manasseh 1')
+        self.assertEqual(f('manasseh 1'),
+                         'Prayer of Manasseh 1')
 
     def test_1_maccabees(self):
-        # TODO
-        raise Exception('Not yet implemented')
+        self.assertEqual(f('1 maccabees 1:1'), 'I Maccabees 1:1')
+        self.assertEqual(f('1Macc 1:1'), 'I Maccabees 1:1')
+        self.assertEqual(f('I Macc 1:1'), 'I Maccabees 1:1')
+        self.assertEqual(f('I Maccabees 1:1'), 'I Maccabees 1:1')
 
     def test_2_maccabees(self):
-        # TODO
-        raise Exception('Not yet implemented')
+        self.assertEqual(f('2 maccabees 1:1'), 'II Maccabees 1:1')
+        self.assertEqual(f('2Macc 1:1'), 'II Maccabees 1:1')
+        self.assertEqual(f('II Macc 1:1'), 'II Maccabees 1:1')
+        self.assertEqual(f('II Maccabees 1:1'), 'II Maccabees 1:1')
 
--- scriptures/texts/deuterocanon.py
+++ scriptures/texts/deuterocanon.py
@@ -1,26 +1,89 @@
 from __future__ import unicode_literals
 from .base import Text
 
+
 class Deuterocanon(Text):
     """
     Deuterocanonical Books
     """
     books = {
-        'tob': ('Tobit', 'Tob', 'Tob(?:it)?', [22, 14, 17, 21, 22, 17, 18, 21, 6, 12, 19, 22, 18, 15,]),
+        '1esd': ('I Esdras', '1Esd', '(?:1|I) ?Esd(?:ras)?',
+                 [
+                     58, 39, 24, 63, 73, 34, 15, 96, 55
+                 ]),
+        
+        '2esd': ('II Esdras', '2Esd', '(?:2|II) ?Esd(?:ras)?',
+                 [
+                     40, 48, 36, 52, 56, 59, 140, 63, 47,
+                     59, 46, 51, 58, 48, 63, 78
+                 ]),
+        
+        'tob': ('Tobit', 'Tob', 'Tob(?:it)?',
+                [22, 14, 17, 21, 22, 17, 18, 21, 6, 12, 19, 22, 18, 15]),
 
-        # TODO:
-        # Judith
-        # Additions to Esther
-        # Wisdom of Solomon
-        # Ecclesiasticus (aka Sirach)
-        # Baruch
-        # Letter of Jeremiah
-        # Prayer of Azariah
-        # Susanna
-        # Bel and the Dragon
-        # Prayer of Manasseh
-        # 1 Maccabees
-        # 2 Maccabees
+        'jdt': ('Judith', 'Jdt', 'Jud(?:ith)?',
+                [
+                    16, 28, 10, 15, 24, 21, 32, 35, 14,
+                    23, 23, 20, 20, 19, 13, 25
+                ]),
 
-        }
+        'addesth': ('Esther (Greek)', 'AddEsth', 'Esther \\(Greek\\)?',
+                    [
+                        39, 23, 22, 47, 28, 14, 10, 41, 32, 13
+                    ]),
 
+        'wis': ('The Wisdom of Solomon', 'Wis',
+                '(?:The )?Wis(?:dom(?: of Solomon)?)?',
+                [
+                    16, 24, 19, 20, 23, 25, 30, 20, 18,
+                    21, 26, 27, 19, 31, 19, 29, 21,
+                    25, 22
+                ]),
+
+        'sir': ('Sirach', 'Sir', 'Sir(?:ach)?',
+                [
+                    30, 18, 31, 31, 15, 37, 36, 19, 18,
+                    31, 34, 18, 26, 27, 20, 30, 32, 33,
+                    30, 31, 28, 27, 34, 26, 29, 30,
+                    26, 28, 25, 31, 24, 31, 26, 20,
+                    26, 31, 34, 35, 30, 23, 25, 33,
+                    23, 26, 20, 25, 25, 16, 29, 30
+                ]),
+
+        'bar': ('Baruch', 'Bar', 'Bar(?:uch)?',
+                [
+                    21, 35, 37, 37, 9
+                ]),
+
+        'epjer': ('Letter of Jeremiah', 'EpJer', 'Letter of Jeremiah',
+                  [73]),
+
+        'prayazariah': ('Prayer of Azariah', 'prayazariah',
+                        '(?:prayer of )?Azar(?:iah)?',
+                        [68]),
+
+        'susanna': ('Susanna', 'susanna', 'susanna', [64]),
+        
+        'beldragon': ('Bel and the Dragon', 'beldragon',
+                      'bel(?:(?: and the)? dragon)?',
+                      [42]),
+
+        'praymanasseh': ('Prayer of Manasseh', 'praymanasseh',
+                         '(?:prayer of )?manasseh', [1]),
+
+        '1macc': ('I Maccabees', '1Macc', '(?:1|I) ?Macc(?:abees)?',
+                  [
+                      64, 70, 60, 61, 68, 63, 50, 32, 73,
+                      89, 74, 53, 53, 49, 41, 24
+                  ]),
+
+        '2macc': ('II Maccabees', '2Macc', '(?:2|II) ?Macc(?:abees)?',
+                  [
+                      36, 32, 40, 50, 27, 31, 42, 36, 29,
+                      38, 38, 45, 26, 46, 39
+                  ]),
+
+        'prman': ('The Prayer of Manasseh', 'PrMan',
+                  '(?:The )?Prayer(?: of Manasseh)?',
+                  [15]),
+    }
